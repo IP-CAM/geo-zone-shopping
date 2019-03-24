47d9d53ed46fa442254690f46ba72a8b794d06ae
Rahul Verma <rahulverma0008@gmail.com>
2019-03-24 15:02:00 +0530

    added for opencart version 3.x

---
+++ Opencart_V3.x/README.md
@@ -0,0 +1,21 @@
+# Multiple Geo Zone Shipping
+
+## How To Install
+1) In your OpenCart admin panel go to Extensions > Installer
+2) Upload the file downloaded ocmod.zip file
+3) Go to Extensions > Extensions, choose Shipping from extension type
+4) You will see Geo Zone Shipping module there, click the Install button
+5) After installing finished, click on edit button, configure shipping method as you like.
+Congratulations! Multiple Geo Zone Shipping Module is now installed. You can now see these shipping methods on checkout page.
+
+
+## Features
+1. Easy to install and setup
+2. Multiple Shipping Method for any Geo Zone
+3. Set different shipping cost for any Geo Zone
+3. Flat Rate shipping cost supported
+4. Weight Based shipping cost supported
+5. Multiple Language support for Title
+6. Multiple Language support for Shipping Instruction
+7. Individual Tax Class for any geo zone
+8. Does not modify any core file of Opencart. So you can upgrade your Opencart version without losing this extension.
---
+++ Opencart_V3.x/upload/catalog/model/extension/shipping/geo_zone_shipping.php
@@ -0,0 +1,93 @@
+<?php
+class ModelExtensionShippingGeoZoneShipping extends Model {
+
+	function getQuote($address) {
+
+		$this->load->language('extension/shipping/geo_zone_shipping');
+
+		$shipping_methods = $this->config->get('shipping_geo_zone_shipping_methods');
+
+		$cart_total = $this->cart->getSubTotal();
+		$cart_weight = $this->cart->getWeight();
+
+		$quote_data = array();
+
+		foreach($shipping_methods as $method){
+
+			// if method is enable
+			if($method['status']){
+
+				if(($method['cost_type'] == 'flat' && $cart_total >= (float)$method['total'])
+					|| ($method['cost_type'] == 'weight' && $cart_weight >= (float)$method['weight'])) {
+
+					$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$method['geo_zone_id'] . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+
+					if (!$method['geo_zone_id']) {
+						$status = true;
+					} elseif ($query->num_rows) {
+						$status = true;
+					} else {
+						$status = false;
+					}
+
+					if($status){
+
+						$method_name = $method['name'][$this->config->get('config_language_id')];
+						$method_cost = 0;
+
+						if($method['cost_type'] == 'flat'){
+							$method_cost = $method['flat_cost'];
+						} else if($method['cost_type'] == 'weight'){
+							$weight_cost = explode(",", $method['weight_cost']);
+							foreach($weight_cost as $wcost){
+								$wc = explode(":", $wcost);
+
+								if(empty($wc[1])) continue;
+								
+								$weight = $wc[0];
+								$rate = $wc[1];
+								$method_cost = $rate;
+								
+								if($weight >= $cart_weight){
+									break;
+								}
+							}
+
+							// just for debugging purpose
+							$method_name .= ' (Weight: ' . $cart_weight .')';
+						}
+
+						$title = $this->currency->format($this->tax->calculate($method_cost, $method['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+
+						if($method['instruction'][$this->config->get('config_language_id')]){
+							 $title .= ' (' . $method['instruction'][$this->config->get('config_language_id')].')';
+						}
+
+						$quote_data[$method_name] = array(
+							'code'         => 'geo_zone_shipping.'.$method_name,
+							'title'        => $method_name,
+							'cost'         => $method_cost,
+							'tax_class_id' => $method['tax_class_id'],
+							'text'         => $title
+						);
+					}
+				}
+			}
+		}
+
+		$method_data = array();
+
+		if ($quote_data) {
+			$method_data = array(
+				'code'       => 'geo_zone_shipping',
+				'title'      => '',
+				'quote'      => $quote_data,
+				'sort_order' => $this->config->get('shipping_geo_zone_shipping_sort_order'),
+				'error'      => false
+			);
+		}
+
+		return $method_data;
+	}
+}
+
